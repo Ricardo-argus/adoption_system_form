3b501d44e70ed7425118b5bff3b8f7db4dfd8808
Ricardo-hernandes <ricardo.hernandes@argusmedia.com>
2026-02-11 07:35:41 -0300

    new screens

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,13 @@
 // script JAVASCRIPT
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site Toca dos Peludos carregado!");
+
+    // Exemplo: alerta ao enviar formulário
+    const forms = document.querySelectorAll("form");
+    forms.forEach(form => {
+        form.addEventListener("submit", () => {
+            alert("Informações enviadas com sucesso!");
+        });
+    });
+});
